c720eaf733765b1e19b76e7da448178a13292ec2
tbaba <t.baba@aktsk.jp>
2013-10-18 23:26:13 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DmtcMini::Application.config.secret_token = '10dd0318b172c7cd09d82677ab721c2358008d664cbaa96580bdd8e0fb49142da9db0ac1ac2b2ee4da54b7cc53e0d5b7ee5ac89a1cdf609e46836ea8033a8fea'
