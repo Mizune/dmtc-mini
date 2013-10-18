5ba415f7dfa67bd6c37492c187131390e150fe50
Yusuke Tanaka <y.tanaka@aktsk.jp>
2013-10-19 01:11:48 +0900

    from Rails3.2 to Rails4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DmtcMini::Application.config.secret_token = '10dd0318b172c7cd09d82677ab721c2358008d664cbaa96580bdd8e0fb49142da9db0ac1ac2b2ee4da54b7cc53e0d5b7ee5ac89a1cdf609e46836ea8033a8fea'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DmtcMini::Application.config.secret_key_base = 'fd9ab90f8df6e39906efc5beb69de725549325e279c78e2d3a67124bc66ae91ef13ce8d9daf71b616e082306eea6e30ab4e4bc327a5b44c9b55a564ab4a4b344'
